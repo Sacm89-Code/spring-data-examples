212a774a12e6750b26f945c40dfe6617aa2b71f8
MIRGS <36760551+mirgs@users.noreply.github.com>
2021-03-11 16:53:18 +0100

    Update setup.groovy

--- vars/setup.groovy
+++ vars/setup.groovy
@@ -75,20 +75,28 @@ def call(config) {
 			}
 			
 			// Analizamos con SonarQube el proyecto y pasamos los informes generados (test, cobertura, mutation)
-			/*stage('SonarQube analysis') {
-				steps {				
-					withSonarQubeEnv(credentialsId: 'sonarQubeCredenciales', installationName: 'local') {
-						withMaven (maven: 'maven-3.6.3') {
-							sh 'mvn sonar:sonar -f web/pom.xml \
-							-Dsonar.sourceEncoding=UTF-8 \
-							-Dsonar.junit.reportPaths=target/surefire-reports'
+			stage('SonarQube analysis') {
+				steps {	
+					script {
+						println "----------------------------------" 
+						configF = readYaml (file: config)
+						ficheroPom = configF.setup.ficheroPom 
+						println "Fichero pom del proyecto padre: " + ficheroPom
+						println "----------------------------------"  
+						
+						withSonarQubeEnv(credentialsId: 'sonarQubeCredenciales', installationName: 'local') {
+							withMaven (maven: 'maven-3.6.3') {
+								sh 'mvn sonar:sonar -f ' + ficheroPom + ' \
+								-Dsonar.sourceEncoding=UTF-8 \
+								-Dsonar.junit.reportPaths=target/surefire-reports'
+							}
 						}
 					}
 				}
-			}*/
+			}
 			
 			// Esperamos hasta que se genere el QG y fallamos o no el job dependiendo del estado del mismo
-			/*stage("Quality Gate") {
+			stage("Quality Gate") {
 				steps {
 					timeout(time: 5, unit: 'MINUTES') {
 						// Parameter indicates whether to set pipeline to UNSTABLE if Quality Gate fails
@@ -97,47 +105,64 @@ def call(config) {
 						waitForQualityGate abortPipeline: true
 					}
 				}
-			} */       
+			}       
 			
-			/*stage("Nexus") {
+			stage("Nexus") {
 				steps {
-					List proyectsArray2 = ["example", "projection", "querydsl"]
-					
-					for (proyect2 in proyectsArray2) {						
-						
-						pom = readMavenPom file: "web/" + proyect2 + "/pom.xml";
-						filesByGlob = findFiles(glob: "web/" + proyect2 + "/target/*.${pom.packaging}");
-						echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
-						artifactPath = filesByGlob[0].path;
-						artifactExists = fileExists artifactPath;
-						if(artifactExists) {
-							echo "*** File: ${artifactPath}, group: ${pom.parent.groupId}, packaging: ${pom.packaging}, version ${pom.parent.version}";
-							nexusArtifactUploader(
-								nexusVersion: NEXUS_VERSION,
-								protocol: NEXUS_PROTOCOL,
-								nexusUrl: NEXUS_URL,
-								groupId: pom.groupId,
-								version: pom.parent.version,
-								repository: NEXUS_REPOSITORY,
-								credentialsId: NEXUS_CREDENTIAL_ID,
-								artifacts: [
-									[artifactId: pom.artifactId,
-									classifier: '',
-									file: artifactPath,
-									type: pom.packaging],
-									[artifactId: pom.artifactId,
-									classifier: '',
-									file: "pom.xml",
-									type: "pom"]
-								]
-							);
-						} else {
-							error "*** File: ${artifactPath}, could not be found";
+					script {
+						println "----------------------------------" 
+						configF = readYaml (file: config)
+						println "Configuraciones para utilizar Nexus"
+						proyectsArray2 = configF.nexus.proyectsArray2
+						println "Lista de arrays de los subproyectos: " + proyectsArray2
+						NEXUS_VERSION = configF.nexus.NEXUS_VERSION
+						println "Version Nexus: " + NEXUS_VERSION
+						NEXUS_PROTOCOL = configF.nexus.NEXUS_PROTOCOL
+						println "Protocolo Nexus: " + NEXUS_PROTOCOL
+						NEXUS_URL = configF.nexus.NEXUS_URL
+						println "URL Nexus: " + NEXUS_URL
+						NEXUS_REPOSITORY = configF.nexus.NEXUS_REPOSITORY
+						println "Reporitorio Nexus: " + NEXUS_REPOSITORY
+						NEXUS_CREDENTIAL_ID = configF.nexus.NEXUS_CREDENTIAL_ID
+						println "Credenciales Nexus: " + NEXUS_CREDENTIAL_ID
+						println "----------------------------------"  
+												
+						for (proyect2 in proyectsArray2) {						
+							
+							pom = readMavenPom file: "web/" + proyect2 + "/pom.xml";
+							filesByGlob = findFiles(glob: "web/" + proyect2 + "/target/*.${pom.packaging}");
+							echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
+							artifactPath = filesByGlob[0].path;
+							artifactExists = fileExists artifactPath;
+							if(artifactExists) {
+								echo "*** File: ${artifactPath}, group: ${pom.parent.groupId}, packaging: ${pom.packaging}, version ${pom.parent.version}";
+								nexusArtifactUploader(
+									nexusVersion: NEXUS_VERSION,
+									protocol: NEXUS_PROTOCOL,
+									nexusUrl: NEXUS_URL,
+									groupId: pom.groupId,
+									version: pom.parent.version,
+									repository: NEXUS_REPOSITORY,
+									credentialsId: NEXUS_CREDENTIAL_ID,
+									artifacts: [
+										[artifactId: pom.artifactId,
+										classifier: '',
+										file: artifactPath,
+										type: pom.packaging],
+										[artifactId: pom.artifactId,
+										classifier: '',
+										file: "pom.xml",
+										type: "pom"]
+									]
+								);
+							} else {
+								error "*** File: ${artifactPath}, could not be found";
+							}
 						}
 					}
 				
 				}
-			}*/
+			}
 			
 		}
 	}
