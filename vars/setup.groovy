154962a92493ce85952c307d01b614fac755122e
MIRIAM GARCIA SANCHEZ <0017394@viewnext.com>
2021-03-16 12:44:59 +0100

    Archivos para subir imagen docker a nexus

--- vars/setup.groovy
+++ vars/setup.groovy
@@ -60,13 +60,12 @@ def call(config) {
 						//nexusPublisher nexusInstanceId: 'nexus_local', nexusRepositoryId: 'spring-data-example-dockerfile', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'spring-data-examples']], mavenCoordinate: [artifactId: 'spring-data-examples', groupId: 'org.springframework.data.examples', packaging: 'jar', version: '2.0.0.BUILD-SNAPSHOT']]], tagName: '2.0-SNAPSHOT' 
 						//nexusPublisher nexusInstanceId: 'nexus_local', nexusRepositoryId: 'spring-data-example-dockerfile', packages: []
 						
-						//sh 'docker login -u admin -p sinensia1 http://192.168.1.57:9084/repository/spring-data-example-dockerfile/'
+						//sh 'docker login -u admin -p sinensia1 https:192.168.1.57:9084/repository/spring-data-example-dockerfile/'
 						//sh 'docker push ' + NEXUS_URL + '/repository/' + NEXUS_REPOSITORY  + 'spring-data-examples:2.0-SNAPSHOT'
 						//sh 'docker push ' + NEXUS_URL + '/' + NEXUS_REPOSITORY  + 'spring-data-examples:2.0-SNAPSHOT'						
 						//sh 'docker push 192.168.1.57:9084/repository/spring-data-example-dockerfile/spring-data-examples'	
 						sh 'docker image ls'
-						//sh 'docker tag spring-data-examples:2.0-SNAPSHOT 192.168.1.57:9084/spring-data-examples:2.0-SNAPSHOT'						
-						//sh 'docker push 192.168.1.57:9084/spring-data-examples:2.0-SNAPSHOT'						
+						sh 'docker tag spring-data-examples:2.0-SNAPSHOT 192.168.1.57:9084/spring-data-examples:2.0-SNAPSHOT'						
 						sh 'docker push 192.168.1.57:9084/spring-data-examples:2.0-SNAPSHOT'
 						
 					}
